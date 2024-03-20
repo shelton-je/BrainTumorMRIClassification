985589b55b6b14408da4a1b3d57772905a37300d
Jerem <Jeremyshelton18@gmail.com>
2024-03-19 22:28:05 -0400

    starting to work on the training pipepline

--- PGGANTrain.py
+++ PGGANTrain.py
@@ -16,42 +16,38 @@ import matplotlib.animation as animation
 
 
 def main():
-    print(os.getcwd())
     data_dir = 'Brain_MRI_Images\Train'
 
-    transform = transforms.Compose([
+    # load and preproccess the data
+    dataset = dset.ImageFolder(data_dir, transform=transforms.Compose([
         transforms.Resize(256),
         transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    ])
-
-    dataset = dset.ImageFolder(data_dir, transform=transform)
-    normal_dataset = torch.utils.data.Subset(dataset, range(0, 135))
+    ]))
+    normal_dataset = torch.utils.data.Subset(dataset, range(0, 136))
     normal_dataloader = DataLoader(normal_dataset, batch_size=16, shuffle=True, num_workers=4)
 
-    tumor_dataset = torch.utils.data.Subset(dataset, range(135, 320))
+    tumor_dataset = torch.utils.data.Subset(dataset, range(136, 320))
     tumor_dataloader = DataLoader(tumor_dataset, batch_size=16, shuffle=True, num_workers=4)
 
-    # Decide which device we want to run on
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    # Plot some training images
-    real_batch = next(iter(tumor_dataloader))
-    plt.figure(figsize=(8,8))
-    plt.axis("off")
-    plt.title("Training Images")
-    plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(),(1,2,0)))
-    plt.show()
-    plt.close()
-
-    # Plot some training images
-    real_batch = next(iter(normal_dataloader))
-    plt.figure(figsize=(8,8))
-    plt.axis("off")
-    plt.title("Training Images")
-    plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(),(1,2,0)))
-    plt.show()
-    plt.close()
+    #device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # # Plot some training images
+    # real_batch = next(iter(tumor_dataloader))
+    # plt.figure(figsize=(8,8))
+    # plt.axis("off")
+    # plt.title("Training Images")
+    # plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(),(1,2,0)))
+    # plt.show()
+    # plt.close()
+    #
+    # # Plot some training images
+    # real_batch = next(iter(normal_dataloader))
+    # plt.figure(figsize=(8,8))
+    # plt.axis("off")
+    # plt.title("Training Images")
+    # plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(),(1,2,0)))
+    # plt.show()
+    # plt.close()
 
 if __name__ == '__main__':
     main()
