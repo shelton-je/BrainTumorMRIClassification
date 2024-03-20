a6be35c6744947a432f8b2f1a88c44bb00e86e12
Jerem <Jeremyshelton18@gmail.com>
2024-03-19 20:50:23 -0400

    test commit

--- PGGANTrain.py
+++ PGGANTrain.py
@@ -42,6 +42,16 @@ def main():
     plt.title("Training Images")
     plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(),(1,2,0)))
     plt.show()
+    plt.close()
+
+    # Plot some training images
+    real_batch = next(iter(normal_dataloader))
+    plt.figure(figsize=(8,8))
+    plt.axis("off")
+    plt.title("Training Images")
+    plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(),(1,2,0)))
+    plt.show()
+    plt.close()
 
 if __name__ == '__main__':
     main()
